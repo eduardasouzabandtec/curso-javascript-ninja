839e53c11c800ee9b99a4a738e0dc51f4ac4e9b7
eduardasouzabandtec <eduarda.souza@bandtec.com.br>
2022-02-16 22:58:48 -0300

    feat(day5): challenge 12 and 13

--- challenge-12/challenge-12.js
+++ challenge-12/challenge-12.js
@@ -1,3 +1,6 @@
+(function(){
+
+
 /*
 Envolva todo o conteúdo desse arquivo em uma IIFE.
 */
@@ -12,17 +15,25 @@ de valor para cada propriedade.
 */
 // ?
 console.log( 'Propriedades de "person":' );
-
+ var person = {
+    "name": "Eduarda",
+    "lastname": "Souza",
+    "age": 21
+ }
 /*
 Mostre no console, em um array, todas as propriedades do objeto acima.
 Não use nenhuma estrutura de repetição, nem crie o array manualmente.
 */
 // ?
+var propriedades = Object.keys(person);
+console.log(propriedades);
 
 /*
 Crie um array vazio chamado `books`.
 */
 // ?
+
+var book = [];
 
 /*
 Adicione nesse array 3 objetos, que serão 3 livros. Cada livro deve ter a
@@ -32,41 +43,63 @@ seguintes propriedades:
 */
 // ?
 console.log( '\nLista de livros:' );
+var livro1 = {
+    "name": "pai rico",
+    "pages": "500"
+};
+var livro2 = {
+    "name": "codigo limpo",
+    "pages": "1000"
+};
+var livro3 = {
+    "name": "minutos de sabedoria",
+    "pages": "300"
+};
+book.push(livro1);
+book.push(livro2);
+book.push(livro3);
 
 /*
 Mostre no console todos os livros.
 */
 // ?
 
+console.log(book);
+
 console.log( '\nLivro que está sendo removido:' );
 /*
 Remova o último livro, e mostre-o no console.
 */
 // ?
+var livroRemovido = book.pop();
+console.log(livroRemovido);
+
 
 console.log( '\nAgora sobraram somente os livros:' );
 /*
 Mostre no console os livros restantes.
 */
 // ?
-
+console.log(book);
 /*
 Converta os objetos que ficaram em `books` para strings.
 */
 // ?
 console.log( '\nLivros em formato string:' );
 
+var livros = JSON.stringify(book);
 /*
 Mostre os livros nesse formato no console:
 */
 // ?
-
+console.log(livros)
 /*
 Converta os livros novamente para objeto.
 */
 // ?
 console.log( '\nAgora os livros são objetos novamente:' );
-
+var livrinho = JSON.parse(livros);
+console.log(livrinho);
 /*
 Mostre no console todas as propriedades e valores de todos os livros,
 no formato abaixo:
@@ -74,27 +107,40 @@ no formato abaixo:
 */
 // ?
 
+book.forEach(function (livro) {
+    console.log(livro)      
+})
+
+
 /*
 Crie um array chamado `myName`. Cada item desse array deve ser uma letra do
 seu nome. Adicione seu nome completo no array.
 */
 // ?
+var myName = ["E", "D", "U", "A", "R", "D", "A"];
 console.log( '\nMeu nome é:' );
 
 /*
 Juntando todos os itens do array, mostre no console seu nome.
 */
 // ?
+console.log(myName.join(""))
 
 console.log( '\nMeu nome invertido é:' );
+
+var nomeInvertido = myName.reverse();
 
 /*
 Ainda usando o objeto acima, mostre no console seu nome invertido.
 */
 // ?
+console.log(nomeInvertido.join(""))
 
 console.log( '\nAgora em ordem alfabética:' );
 /*
 Mostre todos os itens do array acima, odenados alfabéticamente.
 */
 // ?
+var ordem = myName.sort();
+ console.log(ordem.join(""))
+})()
--- challenge-13/challenge-13.js
+++ challenge-13/challenge-13.js
@@ -1,3 +1,6 @@
+(function(){
+
+
 /*
 Envolva todo o código desse desafio em uma IIFE.
 */
@@ -8,6 +11,8 @@ usando o método visto na aula 13.
 */
 console.log( 'O array em formato de string é:' );
 // ?
+var myArray = [1, 2, 3, 4];
+console.log(myArray.toString())
 
 /*
 Crie 2 arrays `sul` e `sudeste`, que serão as regiões do Brasil.
@@ -15,24 +20,33 @@ Cada array deve conter os estados dessa região.
 */
 // ?
 
+var sul = ["Santa Catarina", "Parana", "Rio Grande Do Sul"];
+var sudeste = ["São Paulo", "Rio De Janeiro", "Minas Gerais", "Espirito Santo"];
 /*
 Crie uma variável chamada `brasil`, que irá receber as duas regiões
 concatenadas. Mostre o `brasil` no console.
 */
 console.log( '\nAlguns Estados do Brasil:' );
 // ?
+var brasil = sul.concat(sudeste);
+
+console.log(brasil)
 
 /*
 Adicione 3 novos estados da região Norte no início do array e mostre no console.
 */
 console.log( '\nMais estados adicionados:' );
 // ?
+brasil.unshift("Acre", "Para", "Amazonas");
 
+console.log(brasil)
 /*
 Remova o primeiro estado do array `brasil` e mostre-o no console.
 */
 console.log( '\nEstado removido:' );
 // ?
+brasil.shift();
+console.log(brasil)
 
 /*
 Crie um novo array chamado `newSul`, que receba somente os estados do sul,
@@ -40,28 +54,30 @@ pegando do array `brasil`. Não remova esses itens de `brasil`.
 */
 // ?
 
+var newSul = brasil.slice(2,5);
 /*
 Mostre no console os estados que estão em `newSul`.
 */
 console.log( '\nEstados do Sul do Brasil:' );
 // ?
-
+console.log(newSul);
 /*
 Mostre no console todos os estados que estão em `brasil`.
 */
 console.log( '\nAlguns Estados do Brasil:' );
 // ?
-
+console.log(brasil);
 /*
 Crie um novo array chamado `nordeste`, que tenha os estados do nordeste.
 */
 // ?
-
+var nordeste = ["Alagoas", "Bahia", "Pernambuco", "Ceará", "Maranhão", "Paraiba", "Piaui", "Rio Grande do Norte"];
 /*
 Mostre no console os estados do nordeste.
 */
 console.log( '\nEstados do Nordeste:' );
 // ?
+console.log(nordeste)
 
 /*
 Remova de `brasil` os estados do `sudeste`, colocando-os em uma variável
@@ -69,24 +85,30 @@ chamada `newSudeste`.
 */
 // ?
 
+var newSudeste = brasil.splice(5);
+
 /*
 Adicione os estados do `nordeste` ao array `brasil`. Esses estados devem
 ficar no mesmo nível que os estados já existentes, não em um array separado.
 */
 // ?
+nordeste.forEach((estado) => {
+    brasil.push(estado);
+})
 
 /*
 Mostre no console os estados em `newSudeste`.
 */
 console.log( '\nEstados em newSudeste:' );
 // ?
+console.log(newSudeste)
 
 /*
 Mostre no console os estados do `brasil`.
 */
 console.log( '\nAlguns estados do Brasil:' );
 // ?
-
+console.log(brasil)
 /*
 usando forEach, percorra o array `brasil` e gere um novo array chamado
 `newBrasil`. Esse array deve ter cada item como um objeto, com as
@@ -95,13 +117,19 @@ propriedades:
 - `estado`: que será o estado do array `brasil`.
 */
 // ?
+var newBrasil = [];
+
+brasil.forEach((estado, index) => {
+    newBrasil.push({"id": index , "estado" : estado});
+
+})
 
 /*
 Mostre o array `newBrasil` no console
 */
 console.log( '\nnewBrasil:' );
 // ?
-
+console.log(newBrasil)
 /*
 Percorra o array `brasil` e verifique se os estados tem mais de 7 letras cada,
 atribuindo o resultado à uma variável. Se tiver, mostre no console a frase:
@@ -111,7 +139,10 @@ Senão, mostre no console:
 */
 console.log( '\nTodos os estados de `brasil` tem mais de 7 letras?' );
 // ?
-
+var temSeteLetras = brasil.every((estado) => {
+    return estado.length >= 7;
+})
+console.log(temSeteLetras? "Sim, todos os estados tem mais de 7 letras!" : "Nem todos os estados tem mais de 7 letras!" );
 /*
 Percorra o array `brasil` e verifique se o Ceará está incluído, atribuindo o
 resultado à uma variável. Se esse estado existir no array, mostrar a frase no
@@ -122,7 +153,11 @@ Senão, mostrar a frase:
 */
 console.log( '\nCeará está incluído em `brasil`?' );
 // ?
+var temCeara = brasil.some((estado) => {
+   return estado === "Ceará"
+})
 
+console.log(temCeara? "Ceará está incluído!" : "Ceará não foi incluído :(" );
 /*
 Percorra o array `newBrasil` e crie um novo array que some 1 no ID de cada
 objeto desse array, e adicione a frase abaixo na propriedade `estado`:
@@ -130,21 +165,35 @@ objeto desse array, e adicione a frase abaixo na propriedade `estado`:
 Atribua o novo array a uma variável chamada `map`.
 */
 // ?
-
+var map = newBrasil.map((estados) => {
+    console.log(estados.id)
+    return {
+        id:  estados.id++,
+        estado: `${estados.estado} pertence ao Brasil`
+    }
+   
+   
+})
 /*
 Mostre no console o array criado acima:
 */
 console.log( '\nnewBrasil agora com mais informações:' );
 // ?
+console.log(map)
 
 /*
 Filtre o array criado acima, retornando somente os estados que tiverem
 ID par. Atribua o valor à uma variável chamada `filter`.
 */
 // ?
+var filter = map.filter(( estado) => {
+    return estado.id % 2 == 0;
+})
 
 /*
 Mostre o array filtrado acima no console.
 */
 console.log( '\nEstados com ID par:' );
 // ?
+console.log(filter)
+})()
