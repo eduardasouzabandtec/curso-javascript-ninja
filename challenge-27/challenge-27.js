d7b9c64f9b8e2b5a64be9dfe608c8369c274f140
eduardasouzabandtec <eduarda.souza@bantec.com.br>
2022-03-10 01:25:05 -0300

    feat(challenge): add challenge 7 about lib of DOM

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -16,6 +16,85 @@ DOM.isFunction(function() {}); // true
 DOM.isNumber('numero'); // false
 
 Crie os seguintes métodos para verificação de tipo:
-- isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
+- 
 O método isNull deve retornar `true` se o valor for null ou undefined.
 */
+
+(function () {
+    'use strict'
+
+    function DOM(elementOfHTML) {
+        this.element = Array.from(document.querySelectorAll(elementOfHTML));
+        this.on = function on(typeOfEvent, callback) {
+            this.element.forEach((elementHTML) => {
+                elementHTML.addEventListener(typeOfEvent, callback)
+            })
+        };
+        this.off = function off(typeOfEvent) {
+            this.element.forEach((elementHTML) => {
+                console.log("exclui")
+                elementHTML.removeEventListener(typeOfEvent, null)
+            })
+        }
+        this.get = function get() {
+            return this.element;
+        }
+        this.forEach = function forEach() {
+            return Array.prototype.forEach.apply(this.element, arguments);
+        }
+        this.map = function map() {
+            return Array.prototype.map.apply(this.element, arguments);
+        }
+        this.reduce = function reduce () {
+            return Array.prototype.reduce.apply(this.element, arguments)
+        }
+        this.reduceRight = function reduceRight () {
+            return Array.prototype.reduceRight.apply(this.element, arguments)
+        }
+        this.every = function every () {
+            return Array.prototype.every.apply(this.element, arguments)
+        }
+        this.some = function some () {
+            return Array.prototype.some.apply(this.element, arguments)
+        }
+        this.isArray = function isArray(parametro) {
+            return Object.prototype.toString.call(parametro) === "[object Array]"
+        }
+        this.isObject = function isObject(parametro){
+            return Object.prototype.toString.call(parametro) === "[object Object]"
+        }
+        this.isFunction = function isFunction(parametro){
+            return Object.prototype.toString.call(parametro) === "[object Function]"
+        }
+        this.isNumber = function isNumber(parametro){
+            return Object.prototype.toString.call(parametro) === "[object Number]"
+        }
+        this.isString = function isString(parametro){
+            return Object.prototype.toString.call(parametro) === "[object String]"
+        }
+        this.isBoolean = function isBoolean(parametro){
+            return Object.prototype.toString.call(parametro) === "[object Boolean]"
+        }
+        this.isNull = function isNull(parametro){
+            return Object.prototype.toString.call(parametro) === "[object Null]" || Object.prototype.toString.call(parametro) === "[object Undefined]"
+        }
+
+    }
+
+    const dom1 = new DOM("[data-js='link']");
+    console.log(dom1)
+    dom1.forEach(function (item){
+        console.log("item", item);
+    })
+    dom1.map(function (item) {
+        console.log("map", item)
+    })
+    const dom2 = new DOM();
+    console.log(dom2.isArray([1,2,3]))
+    console.log(dom2.isObject({}))
+    console.log(dom2.isFunction(function myFuction(){}))
+    console.log(dom2.isNumber("numero"))
+    console.log(dom2.isNull(null))
+    console.log(dom2.isNull(undefined))
+   
+})()
