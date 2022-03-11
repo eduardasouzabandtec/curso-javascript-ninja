4ec909d48facd30d94833f69ed95b6db59a8404c
eduardasouzabandtec <eduarda.souza@bandtec.com.br>
2022-03-11 00:22:57 -0300

    feat(challenge): add challenge about search for zip code

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,3 +1,6 @@
+(function () {
+  'use strict'
+
   /*
   No HTML:
   - Crie um formulário com um input de texto que receberá um CEP e um botão
@@ -25,3 +28,120 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
+
+  function DOM(elementOfHTML) {
+    this.element = Array.from(document.querySelectorAll(elementOfHTML));
+    this.on = function on(typeOfEvent, callback) {
+      this.element.forEach((elementHTML) => {
+        elementHTML.addEventListener(typeOfEvent, callback)
+      })
+    };
+    this.off = function off(typeOfEvent) {
+      this.element.forEach((elementHTML) => {
+        elementHTML.removeEventListener(typeOfEvent, null)
+      })
+    }
+    this.get = function get() {
+      return this.element;
+    }
+    this.forEach = function forEach() {
+      return Array.prototype.forEach.apply(this.element, arguments);
+    }
+    this.map = function map() {
+      return Array.prototype.map.apply(this.element, arguments);
+    }
+    this.reduce = function reduce() {
+      return Array.prototype.reduce.apply(this.element, arguments)
+    }
+    this.reduceRight = function reduceRight() {
+      return Array.prototype.reduceRight.apply(this.element, arguments)
+    }
+    this.every = function every() {
+      return Array.prototype.every.apply(this.element, arguments)
+    }
+    this.some = function some() {
+      return Array.prototype.some.apply(this.element, arguments)
+    }
+    this.isArray = function isArray(parametro) {
+      return Object.prototype.toString.call(parametro) === "[object Array]"
+    }
+    this.isObject = function isObject(parametro) {
+      return Object.prototype.toString.call(parametro) === "[object Object]"
+    }
+    this.isFunction = function isFunction(parametro) {
+      return Object.prototype.toString.call(parametro) === "[object Function]"
+    }
+    this.isNumber = function isNumber(parametro) {
+      return Object.prototype.toString.call(parametro) === "[object Number]"
+    }
+    this.isString = function isString(parametro) {
+      return Object.prototype.toString.call(parametro) === "[object String]"
+    }
+    this.isBoolean = function isBoolean(parametro) {
+      return Object.prototype.toString.call(parametro) === "[object Boolean]"
+    }
+    this.isNull = function isNull(parametro) {
+      return Object.prototype.toString.call(parametro) === "[object Null]" || Object.prototype.toString.call(parametro) === "[object Undefined]"
+    }
+
+  }
+
+  const $form = new DOM("[data-js='form']")
+  const $cep = new DOM("[data-js='cep']");
+  const $estado = document.querySelector("[data-js='estado']");
+  const $logradouro = document.querySelector("[data-js='logradouro']");
+  const $bairro = document.querySelector("[data-js='bairro']");
+  const $cidade = document.querySelector("[data-js='cidade']");
+  const $dadosCep = document.querySelector("[data-js='dadosCep']");
+  const $status = document.querySelector("[data-js='carregamento']");
+
+  let responseCep = ""
+
+
+  $form.on("submit", function (element) {
+    element.preventDefault();
+    let cepTratado = $cep.get()[0].value.replace(/[^\d]+/g, '');
+    requestDados(cepTratado);
+  })
+
+  function requestDados(cepTratado) {
+    let ajax = new XMLHttpRequest()
+    ajax.addEventListener("readystatechange", function(event) {
+      $status.value = event.target.readyState
+      
+    })
+    ajax.open("GET", `https://viacep.com.br/ws/${cepTratado}/json/`)
+    ajax.onload = tratarRetorno;
+
+    
+    ajax.send()
+  }
+  function tratarRetorno() {
+    $status.value = " Carregando";
+    //apenas para testar e deixar lento assim observar o "carregando", na tela
+    setTimeout(() => {
+      try {
+        responseCep = JSON.parse(this.responseText)
+        montarDadosNaTela(responseCep)
+      }
+      catch (erro) {
+        $status.value = "ERRO"
+        $status.style.color = "red"
+       alert("Confira se o Seu cep está correto")
+      }
+    }, 500);
+   
+  }
+  function montarDadosNaTela(retorno) {
+    $logradouro.value = retorno.logradouro;
+    $bairro.value = retorno.bairro;
+    $cidade.value = retorno.localidade;
+    $estado.value = retorno.uf
+    $dadosCep.value = retorno.cep;
+    $status.value = "Sucesso";
+    $status.style.color = "green"
+  }
+
+
+
+})()
