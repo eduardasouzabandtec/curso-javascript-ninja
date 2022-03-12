e4566d23721940b2dc75fb41ee98a1130af00e04
eduardasouzabandtec <eduarda.souza@bandtec.com.br>
2022-03-12 05:08:46 -0300

    feat(finished project): add integration with server, get, post and delete

--- challenge-32/challenge-32.js
+++ challenge-32/challenge-32.js
@@ -32,4 +32,101 @@ Após enviar o POST, faça um GET no `server` e atualize a tabela para mostrar o
 Crie uma branch `challenge-32` no seu projeto, envie um pull request lá e cole nesse arquivo a URL
 do pull request.
 */
-console.log('Link do pull request do seu projeto');
+(function () {
+  const $inputImage = document.querySelector("[data-js='image']");
+  const $inputMarca = document.querySelector("[data-js='marca']");
+  const $inputAno = document.querySelector("[data-js='anos']");
+  const $inputPlaca = document.querySelector("[data-js='placa']");
+  const $inputCor = document.querySelector("[data-js='cor']");
+  const $form = document.querySelector("[data-js='form']");
+  const $tbody = document.querySelector("[data-js='dataCarro']");
+
+
+  ajax = new XMLHttpRequest()
+
+  getDataTable()
+  $form.addEventListener("submit", function () {
+    event.preventDefault();
+    postMessage()
+    getDataTable()
+  })
+
+  function postMessage() {
+    ajax.open("POST", "http://localhost:3000/car")
+    ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    var dados = "image=" + $inputImage.value + "&brandModel=" + $inputMarca.value + "&year=" + $inputAno.value + "&plate=" + $inputPlaca.value + "&color=" + $inputCor.value
+
+    try {
+      ajax.send(dados);
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+
+  function getDataTable() {
+    ajax.open("GET", "http://localhost:3000/car")
+    ajax.send();
+    // ajax.addEventListener("readystatechange", function() {
+    //   if (this.readyState === 4) {
+    //     try {
+    //       let dados = JSON.parse(this.responseText)
+    //       $tbody.innerHTML = "";
+    //       dados.forEach((dado) =>{
+    //         addTable(dado)
+    //       })
+    //     } catch (erro) {
+    //       console.log("DADOS", erro)
+    //     }
+    //   }
+    // }, false)
+
+    ajax.onload = function () {
+      try {
+        let dados = JSON.parse(this.responseText)
+        $tbody.innerHTML = "";
+        if (Array.isArray(dados)) {
+          dados.forEach((dado) => {
+            addTable(dado)
+          })
+        }
+      } catch (erro) {
+        console.log("DADOS", erro)
+      }
+    }
+  }
+
+  function addTable(retorno) {
+    var td = `<tr>
+    <td><img src="${retorno.image}"></td>
+    <td>${retorno.brandModel}</td>
+    <td>${retorno.year}</td>
+    <td>${retorno.plate}</td>
+    <td>${retorno.color}</td>
+    <td><button data-js="btnClear" id="${retorno.plate}">Remover</button></td>
+    </tr>`;
+    $tbody.innerHTML += td;
+    addClear()
+  }
+
+  function addClear() {
+    $btnClear = document.querySelectorAll('[data-js="btnClear"]')
+    const btn = Array.from($btnClear)
+    btn.forEach(function (btn) {
+      btn.addEventListener("click", deleteCar)
+    })
+  }
+
+  function deleteCar() {
+    var id = this.getAttribute("id")
+    ajax.open("DELETE", "http://localhost:3000/car")
+    ajax.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    ajax.send(JSON.stringify({"plate": id}))
+  }
+
+
+
+
+
+
+})()
